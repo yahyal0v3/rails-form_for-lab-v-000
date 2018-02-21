6a6dad8418b1b1dc53355994914e30f6d62f7466
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-21 03:15:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(class_params(:title, :room_number))
+    @student = Student.create(student_params(:title, :room_number))
     redirect_to student_path
   end
 
@@ -19,7 +19,7 @@ class StudentsController < ApplicationController
 
   private
 
-	def class_params(*args)
+	def student_params(*args)
 	  params.require(:student).permit(*args)
 	end
 end
